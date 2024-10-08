1168dd09dd38a0ee011ee17618242c89c6076975
WhiteKing010207 <135094615+WhiteKing010207@users.noreply.github.com>
2024-10-08 23:11:53 +0530

    Add files via upload

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    tgId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    isBot: {
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    promptTokens:{
+        type: Number,
+        required: false
+    },
+    completionTokens:{
+        type: Number,
+        required: false
+    }
+},{
+    timestamps: true
+})
+
+export default mongoose.model("User", userSchema)
